385369dcb8a5f8ce2acd28db42828fac1ee0c302
Flavia <flavia.bulad@gmail.com>
2023-01-22 16:39:20 -0300

    feat: add get/sign-up

--- src/main/java/com/tweteroo/api/controller/SignUpController.java
+++ src/main/java/com/tweteroo/api/controller/SignUpController.java
@@ -1,6 +1,9 @@
 package com.tweteroo.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,11 @@ public class SignUpController {
 
     @Autowired
     private SignUpRepository repository;
+
+    @GetMapping
+    public List<SignUp> listAll() {
+        return repository.findAll();
+    }
 
     @PostMapping
     public void Create(@RequestBody SignUpDTO req) {
