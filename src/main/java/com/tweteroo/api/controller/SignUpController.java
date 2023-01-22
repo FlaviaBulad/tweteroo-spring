07bfeff0b4292e2bbe1eb5c52155802aaaa93b99
Flavia <flavia.bulad@gmail.com>
2023-01-22 15:19:39 -0300

    feat: add sign up repository

--- src/main/java/com/tweteroo/api/controller/SignUpController.java
+++ src/main/java/com/tweteroo/api/controller/SignUpController.java
@@ -1,18 +1,24 @@
 package com.tweteroo.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.SignUpDTO;
+import com.tweteroo.api.repository.SignUpRepository;
 
 @RestController
 @RequestMapping("/api/sign-up")
 public class SignUpController {
 
+    @Autowired
+    private SignUpRepository repository;
+
     @PostMapping
     public void Create(@RequestBody SignUpDTO req) {
         System.out.println(req);
+        repository.save(req);
     }
 }
---
+++ src/main/java/com/tweteroo/api/repository/SignUpRepository.java
@@ -0,0 +1,9 @@
+package com.tweteroo.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tweteroo.api.model.SignUp;
+
+public interface SignUpRepository extends JpaRepository<SignUp, Long> {
+    
+}
