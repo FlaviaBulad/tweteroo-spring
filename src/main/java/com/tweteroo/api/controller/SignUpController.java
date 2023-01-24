11ebfe0304f0bab3789534f9bc5936912d8c556a
Flavia <flavia.bulad@gmail.com>
2023-01-24 00:47:47 -0300

    feat: add sign up service

--- src/main/java/com/tweteroo/api/controller/SignUpController.java
+++ src/main/java/com/tweteroo/api/controller/SignUpController.java
@@ -1,33 +1,26 @@
 package com.tweteroo.api.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.SignUpDTO;
-import com.tweteroo.api.model.SignUp;
-import com.tweteroo.api.repository.SignUpRepository;
+import com.tweteroo.api.service.SignUpService;
 
 @RestController
 @RequestMapping("/api/sign-up")
 public class SignUpController {
 
     @Autowired
-    private SignUpRepository repository;
-
-    @GetMapping
-    public List<SignUp> listAll() {
-        return repository.findAll();
-    }
+    private SignUpService service;
 
     @PostMapping
-    public void Create(@RequestBody SignUpDTO req) {
+    public ResponseEntity<String> save(@RequestBody SignUpDTO req) {
         System.out.println(req);
-        repository.save(new SignUp(req));
+        service.save(req);
+        return ResponseEntity.ok().body("OK");
     }
 }
