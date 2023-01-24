1bdd986640fdd0b7ecd6e119b97be089decd9903
Flavia <flavia.bulad@gmail.com>
2023-01-24 13:47:28 -0300

    feat: add tweets model

--- src/main/java/com/tweteroo/api/dto/TweetsDTO.java
+++
@@ -1,8 +0,0 @@
-package com.tweteroo.api.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record TweetsDTO(@NotBlank String username, @NotNull String tweet) {
-
-}
